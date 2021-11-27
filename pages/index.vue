8161ea38b4603d3468a9c4d2120634e74c327514
t-tazy <takumin390511@gmail.com>
2021-11-27 14:07:56 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <p>{{ message }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
